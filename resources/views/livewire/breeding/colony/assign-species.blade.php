31347d54f0b9c9c4b931b60fb9e871edfc4f8d62
mvksastry <mvksastry@gmail.com>
2024-07-14 09:50:35 +0530

    permissions corrections

--- resources/views/livewire/breeding/colony/assign-species.blade.php
+++ resources/views/livewire/breeding/colony/assign-species.blade.php
@@ -7,12 +7,12 @@
 			<div class="container-fluid">
 				<div class="row mb-2">
 					<div class="col-sm-6">
-						<h1 class="m-0">Species: Permissions</h1>
+						<h1 class="m-0">Permissions: Species</h1>
 					</div><!-- /.col -->
 					<div class="col-sm-6">
 						<ol class="breadcrumb float-sm-right">
-							<li class="breadcrumb-item"><a href="/dashboard">Species</a></li>
-							<li class="breadcrumb-item active">Permissions</li>
+							<li class="breadcrumb-item"><a href="/dashboard">Permissions</a></li>
+							<li class="breadcrumb-item active">Species</li>
 						</ol>
 					</div><!-- /.col -->
 				</div><!-- /.row -->
