da88a536be26a34e408a370e32cea6b7a375c4b2
mvksastry <mvksastry@gmail.com>
2024-07-17 08:09:38 +0530

    Rack infos being edited

--- resources/views/livewire/occupancy/rackInfos.blade.php
+++ resources/views/livewire/occupancy/rackInfos.blade.php
@@ -1,10 +1,10 @@
-<div class="p-2">
+<div class="p-2 row">
   @foreach($racks as $rack)
-    <button wire:click.prevent="rackLayout('{{ $rack->rack_id }}')">
-    <div class="text-gray-900 text-center">{{ $rack->rack_name }}</div>
-    
-      <img class="inline m-1" src="{{ asset($rackPath.'/shelf.png') }}" alt="" width="48px" height="48px">
-    </button>
+		<div class="col-sm-2 m-1 border border-primary">
+			<button wire:click.prevent="rackLayout('{{ $rack->rack_id }}')">
+				<div class="text-center">{{ $rack->rack_name }}</div>
+				<img class="inline m-1 border border-primary" src="{{ asset($rackPath.'/shelf.png') }}" alt="" width="48px" height="48px">
+			</button>
+		</div>
   @endforeach
-</div>
-									
+</div>		
