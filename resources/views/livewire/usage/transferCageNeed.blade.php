271020e27ca1c78aba0bba4495da0400705d608d
mvksastry <mvksastry@gmail.com>
2024-11-07 10:52:24 +0530

    small corrections

--- resources/views/livewire/usage/transferCageNeed.blade.php
+++ resources/views/livewire/usage/transferCageNeed.blade.php
@@ -25,7 +25,7 @@
 							</tr>
 							<tr>
 								<td>Project Id</td>
-								<td>{{ $x->project_id }}</td>
+								<td>{{ $x->iaecproject_id }}</td>
 							</tr>
 							<tr>
 								<td>Owner Id</td>
