4c1b4e6614c2d332d00caadfd34c840ffe7d4ceb
mvksastry <mvksastry@gmail.com>
2025-01-21 08:07:05 +0530

    Colony reivew of db entries is being coded.

---
+++ resources/views/livewire/breeding/colony/flexMenuColonyReview.blade.php
@@ -0,0 +1,72 @@
+  <!-- Small boxes (Stat box) -->
+  <div class="row">
+    
+    <!-- .col -->
+    <!-- .col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i wire:clck="mouseDBEntries()" class="fas fa-arrow-circle-right"></i>
+          REVIEW MOUSE DB
+        </button>
+        
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="/mating-entries" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          MATING ENTRIES
+        </button>
+        </a>
+      </div>
+    </div>
+    
+		<!--
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+		<!--
+      <div class="small-box">
+        <a href="/add-litter" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          ADD LITTER
+        </button>
+        </a>
+      </div>
+    </div>
+    -->
+		
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="/litter-entries" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          LITTER ENTRIES
+        </button>
+        </a>
+      </div>
+    </div>
+		
+		<div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="#" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          COMING SOON
+        </button>
+        </a>
+      </div>
+    </div>
+  </div>
+    
+  <!-- ./col -->
+  </div> <!-- /.Small boxes end -->
+  <!-- /.row -->
